4ea7b4cd2fa9c24a8a12139023d2232dce6d8f69
Jeremy Likness <jeremy@jeremylikness.com>
2014-02-08 07:29:52 -0500

    Create angular-zone.js

---
+++ angular-zone.js
@@ -0,0 +1,21 @@
+var main = function() {
+    zone.marker = "main";
+    console.log('getting injector');
+    angular.module('myApp', []).run(function($rootScope, $timeout){
+        zone.marker = "module run";
+        $rootScope.populate = function() {
+            zone.marker = "click";
+            $rootScope.data = "Initializing...";
+            $timeout(function() {
+                zone.marker = "timeout";
+                $rootScope.data = "Done";
+            }, 2000);
+        };
+    });
+    angular.bootstrap(document, ['myApp']);           
+};
+
+zone.fork(profilingZone).run(function() {
+    zone.reset();
+    main();
+});
